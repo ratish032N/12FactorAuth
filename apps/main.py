1916fcad7444eefe9a04a9280ff75b384c1e6701
ratish032N <ratishnyaichyai32@gmail.com>
2025-05-15 21:37:24 +0545

    removed white space

--- apps/main.py
+++ apps/main.py
@@ -31,7 +31,7 @@ def analyze_sentiment(text: str):
     analysis = TextBlob(text)
     sentiment = (
         "positive" if analysis.sentiment.polarity > 0.1 
-        else "negative" if analysis.sentiment.polarity < -0.1 
+        else "negative" if analysis.sentiment.polarity < -0.1
         else "neutral"
     )
     return {"text": text, "sentiment": sentiment}
